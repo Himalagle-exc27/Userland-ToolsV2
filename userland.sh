4eb9a6651fbd7a5b5af56b889b1060d4b1989756
Skyhimabruhv27 <slavaseannextgeneration@gmail.com>
2024-10-02 16:13:41 +0800

    Update userland.sh

--- userland.sh
+++ userland.sh
@@ -47,3 +47,70 @@ then
 clear
 sleep 1
 figlet -f shadow "UBUNTU-OS"
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/A>
+fi
+if [ "$menu" -eq 4 ]
+then
+clear
+sleep 1
+figlet -f shadow "OPENSUSE" | lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXAL>
+fi
+if [ "$menu" -eq 5 ]
+then
+clear
+sleep 1
+figlet -f shadow "BACKBOX-LINUX" | lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/A>
+fi
+if [ "$menu" -eq 6 ]
+then
+clear
+sleep 1
+figlet -f shadow "ALPINE-LINUX" | lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXAL>
+fi
+if [ "$menu" -eq 7 ]
+then
+clear
+sleep 1
+figlet -f shadow "ARCH-LINUX" | lolcat
+pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXAL>
+fi
+if [ "$menu" = 12 ]
+then
+clear
+sleep 1
+figlet -f shadow "BY : SKYHIMA" | lolcat
+echo "Visiting The Owner..."  | lolcat
+xdg-open "https://youtube.com/@himalagle-exc27?si=u0t68ELTsNsJckKR"
+fi
+if [ "$menu" -eq 8 ]
+then
+clear
+sleep 1
+figlet -f shadow "PARROT-OS" | lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/A>
+fi
+if [ "$menu" -eq 9 ]
+then
+clear
+sleep 1
+figlet -f shadow "KALI-LINUX" | lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/A>
+fi
+if [ "$menu" -eq 10 ]
+then
+clear
+sleep 1
+figlet -f shadow "FEDORA" | lolcat
+pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/Andron>
+fi
+
+if [ "$menu" -eq 11 ]
+then
+clear
+sleep 1
+figlet -f shadow "MANJARO-LINUX" | lolcat
+pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/Andron>
+fi
