76d9c267e76fed8bff8a8a1ab6d46db9585f9bec
Skyhimabruhv27 <slavaseannextgeneration@gmail.com>
2024-10-02 16:09:02 +0800

    Update userland.sh

---
+++ userland.sh
@@ -0,0 +1,42 @@
+clear
+sleep 1
+figlet -f shadow "SKYHIMA" | lolcat
+echo "HEAR THE MANKIND POWER IS NOT TO BE ABUSED." | lolcat
+echo
+echo "Skyhima Is Online" | lolcat
+echo
+echo "Menu" | lolcat
+echo
+echo
+echo "1.Debian-OS             2.Void-Linux" | lolcat
+echo
+echo
+echo "3.Ubuntu-OS             4.OpenSUSE" | lolcat
+echo
+echo
+echo "5.BackBox-Linux         6.Alpine-Linux" | lolcat
+echo
+echo
+echo "7.Arch-Linux            8.Parrot-OS" | lolcat
+echo
+echo
+echo "9.Kali-Linux            10.Fedora-OS" | lolcat
+echo
+echo
+echo "11.Manjaro-Linux        12.Visit Owner"| lolcat
+echo
+echo "Pilihlah Beberapa Menu Diatas Dan Jangan Salahgunakan Tools Ini!" | lolcat
+echo
+read -p "Pilih Dude : " menu
+if [ "$menu" -eq 1 ]
+then
+clear
+sleep 1
+figlet -f shadow "DEBIAN" | lolcat
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/A>
+fi
+if [ "$menu" -eq 2 ]
+then
+clear
+sleep 1
+figlet -f shadow "VOID LINUX" | lolcat
